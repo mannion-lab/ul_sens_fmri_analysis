71018ea20af1ab37ee0c69ed740e75cc6e386ed7
Damien Mannion <d.mannion@unsw.edu.au>
2015-03-27 18:41:48 +1100

    -

--- ul_sens_analysis/post.py
+++ ul_sens_analysis/post.py
@@ -4,6 +4,7 @@ import logging
 import fmri_tools.utils
 
 import ul_sens_analysis.config
+import ul_sens_analysis.glm
 import ul_sens_fmri.config
 import runcmd
 
@@ -31,6 +32,8 @@ def run(subj_id, acq_date, post_type):
 
     if post_type == "node_distances":
         node_distances(subj_id, acq_date, conf)
+    elif post_type == "glm":
+        glm(subj_id, acq_date, conf)
 
 
 def node_distances(subj_id, acq_date, conf):
@@ -38,7 +41,8 @@ def node_distances(subj_id, acq_date, conf):
     subj_dir = os.path.join(conf.ana.base_subj_dir, subj_id)
 
     post_dir = os.path.join(subj_dir, conf.ana.post_dir)
-    os.chdir(post_dir)
+    dist_dir = os.path.join(post_dir, "dist")
+    os.chdir(dist_dir)
 
     inf_str = subj_id + "_ul_sens_" + acq_date
 
@@ -127,3 +131,172 @@ def node_distances(subj_id, acq_date, conf):
         comb_cmd.extend(dist_paths)
 
         runcmd.run_cmd(" ".join(comb_cmd))
+
+def glm(subj_id, acq_date, conf):
+    """Run the GLM at each node"""
+
+    subj_dir = os.path.join(conf.ana.base_subj_dir, subj_id)
+
+    post_dir = os.path.join(subj_dir, conf.ana.post_dir)
+    dist_dir = os.path.join(post_dir, "dist")
+    post_glm_dir = os.path.join(post_dir, "glm")
+    os.chdir(post_glm_dir)
+
+    inf_str = subj_id + "_ul_sens_" + acq_date
+
+    for vf in ["upper", "lower"]:
+
+        cond_details = ul_sens_analysis.glm._write_onsets(
+            subj_id=subj_id,
+            acq_date=acq_date,
+            conf=conf,
+            vf=vf,
+            runs_type="all",
+            log_dir=os.path.join(subj_dir, "logs")
+        )
+
+        contrast = []
+
+        for curr_cond in cond_details:
+            if "above" in curr_cond["name"]:
+                curr_str = "+"
+            else:
+                curr_str = "-"
+
+            contrast.append(curr_str + curr_cond["name"])
+
+        contrast = " ".join(contrast)
+
+        contrast_details = [
+            {
+                "label": "above_gt_below",
+                "contrast": contrast
+            }
+        ]
+
+        for hemi in ["lh", "rh"]:
+
+            run_paths = [
+                os.path.join(
+                    subj_dir,
+                    "func",
+                    "run_{n:02d}".format(n=run_num),
+                    "{s:s}-run_{n:02d}-uw-{h:s}_nf.niml.dset".format(
+                        s=inf_str, n=run_num, h=hemi
+                    )
+                )
+                for run_num in range(1, conf.exp.n_runs + 1)
+            ]
+
+            # to write
+            glm_filename = "{s:s}-{v:s}-post_glm-{h:s}.niml.dset".format(
+                s=inf_str, v=vf, h=hemi
+            )
+
+            # to write
+            beta_filename = "{s:s}-{v:s}-post_beta-{h:s}.niml.dset".format(
+                s=inf_str, v=vf, h=hemi
+            )
+
+            mask_path = os.path.join(
+                conf.ana.base_subj_dir,
+                subj_id,
+                conf.ana.loc_glm_dir,
+                "{s:s}-loc_{v:s}-mask-{h:s}_nf.niml.dset".format(
+                    s=inf_str, v=vf, h=hemi
+                )
+            )
+
+            # run the GLM on this visual field location
+            fmri_tools.analysis.glm(
+                run_paths=run_paths,
+                output_dir=post_glm_dir,
+                glm_filename=glm_filename,
+                beta_filename=beta_filename,
+                tr_s=conf.ana.tr_s,
+                cond_details=cond_details,
+                contrast_details=contrast_details,
+                censor_str=conf.ana.censor_str,
+                mask_filename=mask_path,
+                matrix_filename="exp_design_" + vf + "_" + hemi
+            )
+
+            # now to convert the beta weights to percent signal change
+
+            # baseline timecourse
+            bltc_filename = "{s:s}-{v:s}-post_bltc-{h:s}.niml.dset".format(
+                s=inf_str, v=vf, h=hemi
+            )
+
+            # baseline
+            bl_filename = "{s:s}-{v:s}-post_bltc-{h:s}.niml.dset".format(
+                s=inf_str, v=vf, h=hemi
+            )
+
+            # psc
+            psc_filename = "{s:s}-{v:s}-post_psc-{h:s}.niml.dset".format(
+                s=inf_str, v=vf, h=hemi
+            )
+
+            beta_bricks = "[40..$]"
+
+            # check the beta bricks are as expected
+            dset_labels = fmri_tools.utils.get_dset_label(
+                beta_filename + beta_bricks
+            )
+
+            desired_labels = []
+
+            for img_id in conf.exp.img_ids:
+                for src_loc in ["above", "below"]:
+                    desired_labels.append(
+                        vf + "_" + src_loc + "_" + str(img_id) + "#0"
+                    )
+
+            assert dset_labels == desired_labels
+
+            # run the PSC conversion
+            fmri_tools.utils.beta_to_psc(
+                beta_path=beta_filename,
+                beta_bricks=beta_bricks,
+                design_path="exp_design_" + vf + "_" + hemi + ".xmat.1D",
+                bltc_path=bltc_filename,
+                bl_path=bl_filename,
+                psc_path=psc_filename,
+            )
+
+            data_filename = "{s:s}-{v:s}-data-amp.txt".format(
+                s=inf_str, v=vf
+            )
+
+            if os.path.exists(data_filename):
+                os.remove(data_filename)
+
+            cmd = [
+                "3dmaskdump",
+                "-noijk",
+                "-o", data_filename,
+                psc_filename
+            ]
+
+            #runcmd.run_cmd(" ".join(cmd))
+
+            # save the betas as text file also, for exploration / checking
+            b_filename = "{s:s}-{v:s}-beta-amp.txt".format(
+                s=inf_str, v=vf
+            )
+
+            if os.path.exists(b_filename):
+                os.remove(b_filename)
+
+            cmd = [
+               "3dmaskdump",
+                "-noijk",
+                "-o", b_filename,
+                beta_filename
+            ]
+
+            #runcmd.run_cmd(" ".join(cmd))
+
+
+
