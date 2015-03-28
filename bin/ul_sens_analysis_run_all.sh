4c3bc66beff942203e45712da33ee8bcab37cbbb
damien <damien@localhost>
2015-03-28 13:01:21 +1100

    -

---
+++ bin/ul_sens_analysis_run_all.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+ul_sens_analysis p1001 20140714 $1
+ul_sens_analysis p1003 20140922 $1
+ul_sens_analysis p1006 20140728 $1
+ul_sens_analysis p1008 20140714 $1
+ul_sens_analysis p1004 20140929 $1
+ul_sens_analysis p1010 20141103 $1
+ul_sens_analysis p1011 20141020 $1
--- bin/ul_sens_post_analysis_run_all_resid.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-ul_sens_post_analysis p1001 20140714 resid
-ul_sens_post_analysis p1003 20140922 resid
-ul_sens_post_analysis p1006 20140728 resid
-ul_sens_post_analysis p1008 20140714 resid
-ul_sens_post_analysis p1004 20140929 resid
-ul_sens_post_analysis p1010 20141103 resid
-ul_sens_post_analysis p1011 20141020 resid
